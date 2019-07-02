e4f4ec6fc04efeea9b40f74f0020db3d1fd28452
Sean <me@ininger.me>
2019-07-01 22:44:47 -0700

    some refactoring

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,44 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-RANDOM_NUMBER = rand(100)
+random_number = rand(100)
 message = ''
-color = 'blue'
 
-def check_answer(guess)
-  if guess > RANDOM_NUMBER + 5
+def check_answer(guess, number)
+  if guess > number  + 5
     'WAY too high.'
-  elsif guess < RANDOM_NUMBER - 5
+  elsif guess < number - 5
     'Way too low'
-  elsif guess > RANDOM_NUMBER
+  elsif guess > number
     'Too high.'
-  elsif guess < RANDOM_NUMBER
+  elsif guess < number
     'Too low.'
   else
-    "You got it right, the secret number was #{RANDOM_NUMBER}"
+    "You got it right, the secret number was #{number}"
   end
 end
 
+def set_color(guess, number)
+  if guess > number + 5
+    'red'
+  elsif guess < number - 5
+    'red'
+  elsif guess > number
+    'yellow'
+  elsif guess < number
+    'yellow'
+  else
+  'green'
+  end
+end
+
+def reset
+  guesses = 5
+end
 get '/' do
   guess = params['guess'].to_i
-  message = check_answer(guess)
+  message = check_answer(guess, random_number)
+  color = set_color(guess, random_number)
   # throw params.inspect
   erb :index, locals: { number: RANDOM_NUMBER, message: message, color: color }
 end
